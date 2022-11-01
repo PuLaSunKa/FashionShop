47f34d33b68b5ec0497db76b4d07d34390a660ef
TuanNA <tuanaht2001@gmail.com>
2022-11-01 16:50:26 +0700

    Add project files.

---
+++ FashionShop.WebApp/Models/Context.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace FashionShop.WebApp.Models
+{
+    public class Context : DbContext
+    {
+        private string connectionStrings;
+        public Context() : base()
+        {
+            var builder = new ConfigurationBuilder();
+
+            builder.AddJsonFile("appsettings.json", optional: false);
+
+            var configuration = builder.Build();
+
+            connectionStrings = configuration.GetConnectionString("MyConnection").ToString();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionStrings);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<SanPham> SanPhams { get; set; }
+    }
+}
---
+++ FashionShop.WebApp/Models/SanPham.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FashionShop.WebApp.Models
+{
+    [Table("SanPham")]
+    public class SanPham
+    {
+        public int MaSanPham { get; set; }
+        public string? TenSanPham { get; set; }
+        public int MaLoai { get; set; }
+        public int MaNhanHang { get; set; }
+        public string? MoTa { get; set; }
+        public decimal Gia { get; set; }
+        public int SoLuong { get; set; }
+        public string? TieuDe { get; set; }
+        public DateTime NgayTao{ get; set; }
+    }
+}
